d4a5cc8f05343fd8a2bdd938259fae92e9295d67
Eraldo Jaku <eraldo.jaku@outlook.com>
2024-11-06 10:49:07 +0100

    Add empty test

--- Max_Profit_2/go/main_test.go
+++ Max_Profit_2/go/main_test.go
@@ -15,6 +15,7 @@ func TestMaxProfit(t *testing.T) {
 		{name: "Test1", prices: []int{7, 1, 5, 3, 6, 4}, profit: 7},
 		{name: "Test2", prices: []int{1, 2, 3, 4, 5}, profit: 4},
 		{name: "Test3", prices: []int{7, 6, 4, 3, 1}, profit: 0},
+		{name: "Test4", prices: []int{}, profit: 0},
 	}
 
 	for _, tc := range tests {
